90168adefec9d0a02709c07294b08d23973c30da
Hampus <hamp.ohman@gmail.com>
2022-02-22 15:18:14 +0100

    fixed createLogEntry

--- project_2/Patient.java
+++ project_2/Patient.java
@@ -28,7 +28,7 @@ public <LogEntry>logEntry getJournal() {
 
 //skapar en ny entry i patientens log, behövs det en if-sats som kollar om doktorn är behörig att skapa logen?
 public void createLogEntry(Doctor doc, Nurse nurse, String comment){
-    LogEntry newLog = new LogEntry(this.Patient, doc, nurse, comment);
+    LogEntry newLog = new LogEntry(this, doc, nurse, comment);
     journal.add(newLog);
 }
 
