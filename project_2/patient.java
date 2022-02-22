a58fb0a5999311db334c944ff2a796bf20cdc33b
Hampus <hamp.ohman@gmail.com>
2022-02-22 13:25:24 +0100

    pat

--- project_2/patient.java
+++ project_2/patient.java
@@ -11,7 +11,7 @@ public Patient(String name, long id){
 //om vi ska skapa en patient med en log, behövs detta verkligen?
 public Patient(<LogEntry> log, String name, long id){
     super(name, id);
-    this.log = log;
+    journal = log;
 }
 
 public <LogEntry>logEntry getLog() {
@@ -20,7 +20,7 @@ public <LogEntry>logEntry getLog() {
 
 //skapar en ny entry i patientens log, behövs det en if-sats som kollar om doktorn är behörig att skapa logen?
 public void createLogEntry(Doctor doc, Nurse nurse, String comment){
-    LogEntry newLog = new LogEntry(this.Patient, doctor, nurse, comment);
+    LogEntry newLog = new LogEntry(this.Patient, doc, nurse, comment);
     log.add(newLog);
 }
 
