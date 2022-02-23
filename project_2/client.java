9535b757f15e92191a7742c132487ce0170b3b30
Hampus <hamp.ohman@gmail.com>
2022-02-23 15:33:58 +0100

    Added new methods for request and response

--- project_2/client.java
+++ project_2/client.java
@@ -15,7 +15,24 @@ import java.security.cert.*;
  */
 
 public class client {
+
+  private SSLSocket socket;
+
   public static void main(String[] args) throws Exception {
+    client cl = new client();
+    cl.connect(args); //skapar förbindelse med server
+
+    while(true){ 
+      cl.sendReq(); //skickar förfrågningar till servern
+      cl.recieveRes(); //tar emot svar från servern
+      break; //behöver lägga till nånting som bryter loopen, kanske kan sendReq returnera en boolean?
+    }
+
+
+    cl.socket.close(); //stänger ner
+  }
+
+  private void connect(String[] args){
     String host = null;
     int port = -1;
     for (int i = 0; i < args.length; i++) {
@@ -53,7 +70,7 @@ public class client {
       } catch (Exception e) {
         throw new IOException(e.getMessage());
       }
-      SSLSocket socket = (SSLSocket) factory.createSocket(host, port);
+      socket = (SSLSocket) factory.createSocket(host, port);
       System.out.println("\nsocket before handshake:\n" + socket + "\n");
 
       /*
@@ -64,37 +81,93 @@ public class client {
        */
 
       socket.startHandshake();
-      SSLSession session = socket.getSession();
-      Certificate[] cert = session.getPeerCertificates();
-      String subject = ((X509Certificate) cert[0]).getSubjectX500Principal().getName();
-      String issuer = ((X509Certificate) cert[0]).getIssuerX500Principal().getName();
-      System.out.println("certificate name (subject DN field) on certificate received from server:\n" + subject + "\n");
-      System.out.println("issuer (cert issuer DN field): " + issuer);
-      System.out.println("socket after handshake:\n" + socket + "\n");
-      System.out.println("secure connection established\n\n");
+  } catch (Exception e) {
+    e.printStackTrace();
+  }
 
-      BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-      PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-      String msg;
-      for (;;) {
-        System.out.print(">");
-        msg = read.readLine();
+}
+
+  private void sendReq() throws IOException{
+    BufferedReader in = null;
+    ObjectOutputStream out = null;
+    BufferedReader read = null;
+    in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    read = new BufferedReader(new InputStreamReader(System.in));
+    out = new ObjectOutputStream(socket.getOutputStream());
+    System.out.println("For which patient do you want to operate?" + "/n" + ">");
+    String patient = read.readLine();
+    String msg;
+    Request request;
+    System.out.print("What do you want to do?" + "/n" + 
+        "Press 1 to create a file, 2 to write to a file, 3 to read a file, 4 to delete a file and 9 to quit" + "/n" +
+        ">");
+        msg= read.readLine();
         if (msg.equalsIgnoreCase("quit")) {
-          break;
+          break; // fixa denna
         }
-        System.out.print("sending '" + msg + "' to server...");
-        out.println(msg);
-        out.flush();
-        System.out.println("done");
-        System.out.println("received '" + in.readLine() + "' from server\n");
-      }
-      in.close();
-      out.close();
-      read.close();
-      socket.close();
-    } catch (Exception e) {
+        switch(Integer.parseInt(msg)){ //här behöver vi inte göra så mycket mer än att välja metod, när vi fått access gör vi resten
+          //create new file
+          case 1:
+          System.out.println("Which nurse do you want to associate with the log?" + "/n" + ">");
+          String nurse = read.readLine(); //hade velat söka bland våra nurses och se att namnet finns, då kan vi koppla till en nurse ist för string också
+          System.out.println("Log entry:" + "/n" + ">");
+          String comment = read.readLine();
+          request = new Request(patient, "create", nurse); 
+          out.writeObject(request);
+          break;
+
+          //write to existing file
+          case 2:
+          System.out.println("Path to desired file: ");
+          String path = read.readLine();
+          Request readReq = new Request(patient, "read", path);
+          out.writeObject(readReq); //dessa två rader är för att man ska kunna läsa vad som står i filen innan vi skriver på
+          System.out.println(in.readLine()); //behövs nånting som ser till att vi bara läser filer vi sen kan skriva till
+          System.out.println("What do you want to add to the file?" + "/n" + ">");
+          comment = read.readLine(); //borde inte comment behöva initeras? den initieras ju inuti ett annat case?
+          request = new Request(patient, "write",  path, comment);
+          out.writeObject(request);
+          break;
+          
+          //read file
+          case 3:
+          request = new Request(patient, "read"); //tänker nu att alla logs som user har access till skrivs ut, kanske ska välja fil?
+          out.writeObject(request);
+
+          break;
+          
+          //delete file
+          case 4:
+          System.out.println("Path to file that is to be deleted:");
+          path = read.readLine();
+          request = new Request(patient, path); 
+          out.writeObject(request);
+
+          break;
+
+          case 9:
+
+          break;
+
+          default:
+          System.out.println("Please enter enter one of the allowed numbers!");
+          //Här vill vi skicka tillbaka användaren så den får välja om
+
+        }
+  
+}
+
+  private void recieveRes(){
+    try {
+      ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+      Response response = (Response)in.readObject();
+      System.out.println(response.getStatus()); //skriver ut status på response, dvs om den lyckats eller ej
+      // TODO code to write out file if we wanted to read
+
+    } catch (ClassNotFoundException | IOException e) {
+      // TODO Auto-generated catch block
       e.printStackTrace();
     }
+
   }
 }
