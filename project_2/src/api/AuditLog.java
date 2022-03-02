8cca6694fa152f4dbe45b679081655bc90d39444
malinrud <100198435+malinrud@users.noreply.github.com>
2022-03-02 12:02:24 +0100

    Uppdaterat AuditLog.java samt flyttat auditlog.txt

--- project_2/src/api/AuditLog.java
+++ project_2/src/api/AuditLog.java
@@ -1,32 +1,31 @@
-// package api;
+package api;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.io.FileWriter;   // Import the FileWriter class
 import java.io.IOException;  // Import the IOException class to handle errors
-// import java.time.LocalDate;
-// import java.time.format.DateTimeFormatter;
 
-// import server.patient.LogEntry;
+import server.patient.LogEntry;
 
 public class AuditLog { // The list keeping track of all entries and edits
     // String log; // log to keep track of all the log entries
     //private DateTimeFormatter dtf;
+
     private StringBuilder logEntry;
-    LocalDateTime dateTime;
+    private LocalDateTime dateTime;
 
     //action behöver skrivas som t.ex "attempted to write to" "wrote to" "attempted to read" "read"
     public AuditLog(String user, String action, String patient) {
         dateTime = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
         logEntry = new StringBuilder(dateTime.format(formatter)+" "+user+" "+action+" the journal of "+patient+"/n");
-        addToFile(logEntry);
+        addToAuditLog(logEntry);
     }
 
-    private void addToFile(String sb) {
+    private void addToAuditLog(String sb) {
         try {
-            FileWriter auditLogEntry = new FileWriter("auditlog.txt");
-            auditLogEntry.write(sb);
+            FileWriter auditLogEntry = new FileWriter("auditlog.txt", true); // creates filewriter which can append to our auditlog
+            auditLogEntry.append(sb);
             auditLogEntry.close();
             System.out.println("Audit log entry created.");
         } catch (IOException e) {
